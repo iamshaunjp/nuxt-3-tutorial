881cd10ba6fa576dfb7830884fe4064e000692d1
Shaun <shaun@thenetninja.co.uk>
2022-10-14 15:22:11 +0100

    lesson-10

---
+++ error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="mt-7 max-w-sm mx-auto text-center card">
+    <p class="mt-7 text-7xl font-bold">{{error.statusCode}}</p>
+    <p class="mt-7 text-6xl">Ooops.</p>
+    <p class="mt-7">{{error.message}}</p>
+    <button class="btn my-7" @click="handleError">Go Home...</button>
+  </div>
+</template>
+
+<script setup>
+  defineProps({
+    error: Object
+  })
+
+  const handleError = () => clearError({ redirect: '/' })
+</script>
--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -11,6 +11,10 @@
   //  fetch the products
   const { data: product } = await useFetch(uri, { key: id })
 
+  if (!product.value) {
+    throw createError({ statusCode: 404, statusMessage: 'Product not found' })
+  }
+
   definePageMeta({
     layout: "products",
   })
