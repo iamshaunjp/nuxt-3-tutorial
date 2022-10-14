6b13de9e70e33d549e0f910d10ba1350f3091fb6
Shaun <shaun@thenetninja.co.uk>
2022-10-14 13:04:16 +0100

    lesson-7

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <header>
-      <nav>
-        <NuxtLink to="/">Nuxt Dojo</NuxtLink>
-        <ul >
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4 flex justify-between">
+        <NuxtLink to="/" class="font-bold">Nuxt Dojo</NuxtLink>
+        <ul class="flex gap-4">
           <li><NuxtLink to="/">Home</NuxtLink></li>
           <li><NuxtLink to="/about">About</NuxtLink></li>
-          <li><NuxtLink to="/products">Merch</NuxtLink></li>
+          <li><NuxtLink to="/products" class="btn">Merch</NuxtLink></li>
         </ul>
       </nav>
     </header>
-    <div>
+    <div class="container mx-auto p-4">
       <slot />
     </div>
   </div>
--- layouts/products.vue
+++ layouts/products.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    <header>
-      <nav>
-        <NuxtLink to="/products">Nuxt Dojo Merch</NuxtLink>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4">
+        <NuxtLink to="/products" class="font-bold">Nuxt Dojo Merch</NuxtLink>
       </nav>
     </header>
-    <div>
+    <div class="container mx-auto p-4">
       <slot />
     </div>
-    <footer>
-      <ul >
+    <footer class="container mx-auto p-4 flex justify-between border-t-2">
+      <ul class="flex gap-4">
         <li><NuxtLink to="/">Home</NuxtLink></li>
         <li><NuxtLink to="/about">About</NuxtLink></li>
         <li><NuxtLink to="/products">Merch</NuxtLink></li>
