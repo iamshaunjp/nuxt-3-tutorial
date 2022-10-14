7b8a9865e2e81d4e8524644747350a43413e2750
Shaun <shaun@thenetninja.co.uk>
2022-10-14 11:00:37 +0100

    lesson-4

---
+++ pages/products/[id].vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste necessitatibus accusamus architecto dignissimos non harum tenetur rem itaque, dolor aut?</p>
+  </div>
+</template>
+
+<script setup>
+  const { id } = useRoute().params
+</script>
