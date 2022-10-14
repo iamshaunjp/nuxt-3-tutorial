ff99a528e6977561307ec862884fd3701afc0d0e
Shaun <shaun@thenetninja.co.uk>
2022-10-14 13:09:58 +0100

    lesson-8

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <p>Product details for {{ id }}</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste necessitatibus accusamus architecto dignissimos non harum tenetur rem itaque, dolor aut?</p>
+    <p>{{ product.title }}</p>
+    <p>{{ product.price }}</p>
+    <p>{{ product.id }}</p>
   </div>
 </template>
 
 <script setup>
   const { id } = useRoute().params
+  const uri = 'https://fakestoreapi.com/products/' + id
+
+  //  fetch the products
+  const { data: product } = await useFetch(uri, { key: id })
 
   definePageMeta({
     layout: "products",
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
-    <p>Products</p>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="p in products">
+        <NuxtLink :to="`/products/${p.id}`">{{ p.title }}</NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
+  //  fetch the products
+  const { data: products } = await useFetch('https://fakestoreapi.com/products')
+
   definePageMeta({
     layout: "products",
   })
